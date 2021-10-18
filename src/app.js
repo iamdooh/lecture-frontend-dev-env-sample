8fef630fa2a5f002623ed0192456693a58ff37a3
iamdooh <myname520@naver.com>
2021-10-18 16:30:18 +0900

    핫로더 설정 실습

--- src/app.js
+++ src/app.js
@@ -1,24 +1,38 @@
-import axios from 'axios';
-
+import form from './form';
+import result from './result';
 import './app.css';
+
+// import './app.css';
 //import nyancat from './nyancat.jpg';
 
+let formEl;
+let resultEl;
+
 document.addEventListener('DOMContentLoaded', async () => {
-  const res = await axios.get('api/users');
-  console.log(res);
+  formEl = document.createElement('div');
+  formEl.innerHTML = form.render();
+  document.body.appendChild(formEl);
 
-  document.body.innerHTML = (res.data || [])
-    .map((user) => {
-      return `<div>${user.id}: ${user.name}</div>`;
-    })
-    .join('');
-
-  // document.body.innerHTML = `
-  //   <img src="${nyancat}" />
-  // `;
+  resultEl = document.createElement('div');
+  resultEl.innerHTML = await result.render();
+  document.body.appendChild(resultEl);
 });
 
 console.log(process.env.NODE_ENV);
 // console.log(TWO); // 2
 // console.log(TWO_STRING); // "1+1"
 // console.log(api.domain);
+
+if (module.hot) {
+  console.log('핫 모듈 켜짐');
+
+  module.hot.accept('./result', async () => {
+    console.log('result 모듈 변경됨');
+    resultEl.innerHTML = await result.render();
+  });
+
+  module.hot.accept('./form', async () => {
+    console.log('form 모듈 변경됨');
+    formEl.innerHTML = form.render();
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
     before: (app) => {
       app.use(apiMocker('/api', 'mocks/api'));
     },
+    hot: true,
   },
   module: {
     rules: [
